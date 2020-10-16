192f8c9a8ee3083fc1d05df17e16046256d9403a
Saffron Parks <saffron@Saffrons-MacBook-Air.local>
2020-10-16 17:10:12 +0200

    added an interface

--- Dutch Vocab Practice.py
+++ Dutch Vocab Practice.py
@@ -1,3 +1,5 @@
+from tkinter import * 
+from tkinter import messagebox
 from random import choice
 
 dutch = {}
@@ -13,13 +15,44 @@ for line in words:
     dutch[key] = value
     english_keys.append(key)
 
-while True:
-    english_key = choice(english_keys)
-    answer = input("Translate: " + english_key + "\n")
-    dutch_value = dutch[english_key]
+# forming the interface
+window = Tk()
+window.title("Dutch Vocab Practice")
+window.geometry("400x350+470+50")
 
+# the question label
+label_text = StringVar()
+label = Label(window, textvariable = label_text, fg = "#738cbf", font = ("Verdana", 16))
+label.place(x = 80, y = 50)
+
+english_key = ""
+
+def display(label):
+    global english_key
+    english_key = choice(english_keys)
+    label_text.set("Translate: " + english_key)
+
+# the answer text field
+text_variable = StringVar()
+text = Entry(window, textvariable = text_variable)
+text.place(x = 80, y = 100)
+
+def submit():
+    global english_key
+    answer = text.get()
+    dutch_value = dutch[english_key]
     if answer == dutch_value:
-        print("Goed antwoord!")
+        messagebox.showinfo("", "Goed antwoord!")
     else:
-        print("Sorry! The correct answer is: " + dutch_value + ".")
-    print("")
+        messagebox.showinfo("", "Sorry! The correct answer is: " + dutch_value + ".")
+
+    display(label)
+    text_variable.set("")
+
+# the submit button
+button = Button(window, text = "Antwoord", fg = "black", font = ("Verdana", 14), command = submit)
+button.place(x = 80, y = 150)
+
+display(label)
+
+window.mainloop()
